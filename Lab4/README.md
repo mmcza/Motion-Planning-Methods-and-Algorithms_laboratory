a56a0dfc5f6ec743282ddf31900d668561a832fa
mmcza <46852756+mmcza@users.noreply.github.com>
2024-03-23 19:38:23 +0100

    Finished BFS algorithm for Lab4

---
+++ Lab4/README.md
@@ -0,0 +1,33 @@
+# Breadth-First Search (BFS) and Depth-First Search (DFS) 
+
+Put the src directory inside your ros2 workspace.
+
+## Task 1 - Depth-First Search
+
+![DFS Algorithm](/Pictures/Lab4_DFS.gif)
+
+To run the code for the DFS Algorithm (implemented inside [dfs.py](/Lab4/src/mapr_4_student/mapr_4_student/dfs.py) file) you have to type the following commands when being inside your ros2 workspace:
+
+``` colcon build ```
+
+``` source install/setup.bash ```
+
+``` ros2 launch mapr_4_student dfs_launch.py ```
+
+As a result RViz will open and you will see how the DFS Algorithm works.
+
+## Task 2 - Breadth-First Search
+
+![BFS Algorithm](/Pictures/Lab4_BFS.gif)
+
+To run the code for the BFS Algorithm (implemented inside [bfs.py](/Lab4/src/mapr_4_student/mapr_4_student/bfs.py) file) you have to type the following commands when being inside your ros2 workspace:
+
+``` colcon build ```
+
+``` source install/setup.bash ```
+
+``` ros2 launch mapr_4_student bfs_launch.py ```
+
+As a result RViz will open and you will see how the BFS Algorithm works.
+
+The BFS algorithm allows you to find the optimal route to certain point and DFS might do it but only if in each step it goes in correct way.
--- Lab4/src/mapr_4_student/mapr_4_student/bfs.py
+++ Lab4/src/mapr_4_student/mapr_4_student/bfs.py
@@ -7,8 +7,41 @@ class BFS(GridMap):
     def __init__(self):
         super(BFS, self).__init__()
         ###  IF YOU NEED SOME ADDITIONAL FILEDS IN BFS OBJECT YOU CAN INITIALIZED THEM HERE
-
+        self.point_dictionary = {}
+        self.directions = [[0, -1],[-1, 0],[0, 1],[1, 0]]
+        self.previous_iteration = []
+        self.end_point_found = False
     def search(self):
+        if not self.point_dictionary:
+            self.point_dictionary[self.start] = [self.start]
+            self.map.data[self.map.info.width*(self.start[1])+self.start[0]] = 50
+            self.previous_iteration.append(self.start)
+        while not self.end_point_found:
+            points_to_check = self.previous_iteration
+            self.previous_iteration = []
+            for point in points_to_check:
+                for direction in self.directions:
+                    if self.map.data[self.map.info.width*(point[1]+direction[1])+point[0]+direction[0]] == 0:
+                       self.map.data[self.map.info.width*(point[1]+direction[1])+point[0]+direction[0]] = 50
+                       self.point_dictionary[(point[0]+direction[0], point[1]+direction[1])] = point
+                       self.previous_iteration.append((point[0]+direction[0], point[1]+direction[1]))
+                       if self.previous_iteration[-1] == self.end:
+                           self.end_point_found = True
+                           break
+            self.publish_visited()
+            if self.end_point_found:
+                break
+            if not self.previous_iteration:
+                break
+        if self.end_point_found:
+            path = [self.end]
+            last_point = self.end
+            while last_point != self.start:
+                last_point = self.point_dictionary[last_point]
+                path.append(last_point)
+            path.append(self.start)
+            self.publish_path(path)
+
         ### YOUR CODE GOES BELOW
         #
         #
@@ -22,7 +55,7 @@ class BFS(GridMap):
         #
         #
         ### YOUR CODE GOES ABOVE
-        pass  # DELETE THIS LINE IF THE CODE INSERTED
+        #pass  # DELETE THIS LINE IF THE CODE INSERTED
 
 
 def main(args=None):
